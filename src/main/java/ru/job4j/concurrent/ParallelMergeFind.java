bc0ef8330113b5971156f2042d9ab55dec9ed1f5
Tsypa90 <URSA2004@mail.ru>
2022-07-03 13:47:06 +0300

    3. ForkJoinPool [#315067 #318057]

--- src/main/java/ru/job4j/concurrent/ParallelMergeFind.java
+++ src/main/java/ru/job4j/concurrent/ParallelMergeFind.java
@@ -3,14 +3,14 @@ package ru.job4j.concurrent;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelMergeFind extends RecursiveTask<Integer> {
-    private final Object[] array;
-    private final Object elementToSearch;
+public class ParallelMergeFind<T> extends RecursiveTask<Integer> {
+    private final T[] array;
+    private final T elementToSearch;
     private final int from;
     private final int to;
     private static final int OPTIMISTIC = 10;
 
-    private ParallelMergeFind(Object[] array, Object element, int from, int to) {
+    public ParallelMergeFind(T[] array, T element, int from, int to) {
         this.array = array;
         this.elementToSearch = element;
         this.from = from;
@@ -20,37 +20,29 @@ public class ParallelMergeFind extends RecursiveTask<Integer> {
     @Override
     protected synchronized Integer compute() {
         int rsl = -1;
-        if (array.length <= OPTIMISTIC) {
-            for (int i = 0; i < array.length; i++) {
-                if (array[i].equals(elementToSearch)) {
+        if ((to - from) <= OPTIMISTIC) {
+            for (int i = from; i <= to; i++) {
+                if (array[i] == elementToSearch) {
                     rsl = i;
                     return rsl;
                 }
             }
-        }
-        if (to >= from) {
+        } else {
             int mid = (from + to) / 2;
-            ParallelMergeFind leftFind = new ParallelMergeFind(array, elementToSearch, from, mid - 1);
-            ParallelMergeFind rightFind = new ParallelMergeFind(array, elementToSearch, mid + 1, to);
+            System.out.println(Thread.currentThread().getName());
+            ParallelMergeFind<T> leftFind = new ParallelMergeFind<>(array, elementToSearch, from, mid);
+            ParallelMergeFind<T> rightFind = new ParallelMergeFind<>(array, elementToSearch, mid + 1, to);
             leftFind.fork();
             rightFind.fork();
             int left = leftFind.join();
             int right = rightFind.join();
-            if (array[mid].equals(elementToSearch)) {
-                rsl = mid;
-                return rsl;
-            }
-            if (left != -1) {
-                rsl = left;
-            } else if (right != -1){
-                rsl = right;
-            }
+            return Math.max(left, right);
         }
         return rsl;
     }
 
     public static int find(Object[] array, Object element) {
         ForkJoinPool pool = new ForkJoinPool();
-        return pool.invoke(new ParallelMergeFind(array, element, 0, array.length - 1));
+        return pool.invoke(new ParallelMergeFind<>(array, element, 0, array.length - 1));
     }
 }
